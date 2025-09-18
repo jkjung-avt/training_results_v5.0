1f0e5d851a1743e59c54da7d28e3f84a50f49482
JK Jung <jkjung13@gmail.com>
2025-09-18 19:40:45 +0800

    Fix run_test.sh

--- Inventec/benchmarks/bert/implementations/P8000_ngc23.09_pytorch/run_test.sh
+++ Inventec/benchmarks/bert/implementations/P8000_ngc23.09_pytorch/run_test.sh
@@ -8,15 +8,22 @@ python -m torch.distributed.launch --nproc_per_node=8 \
     --target_mlm_accuracy=0.714 \
     --bert_config_path=/workspace/bert_data/phase1/bert_config.json \
     --skip_checkpoint \
+    --eval_iter_start_samples=100000 \
+    --eval_iter_samples=100000 \
+    --eval_batch_size=16 \
+    --eval_dir=/workspace/bert_data/hdf5/eval_varlength \
+    --num_eval_examples=10000 \
+    --cache_eval_data \
     --output_dir=/results \
     --fp16 \
+    --distributed_lamb \
     --allreduce_post_accumulation --allreduce_post_accumulation_fp16 \
     --gradient_accumulation_steps=1 \
     --log_freq=1 \
     --train_batch_size=4 \
     --learning_rate=4e-5 \
     --warmup_proportion=1.0 \
-    --input_dir=/workspace/data_phase2 \
+    --input_dir=/workspace/bert_data/hdf5/training-4320/hdf5_4320_shards_varlength_shuffled \
     --phase2 \
     --max_seq_length=512 \
     --max_predictions_per_seq=76 \
