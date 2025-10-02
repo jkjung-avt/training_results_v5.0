eaf85b13875b7a057478bd35d85a6e9663f686c2
JK Jung <jkjung13@gmail.com>
2025-10-02 15:29:51 +0800

    Add env.sh and chmod +x for all scripts

---
+++ Inventec/benchmarks/llama2_70b_lora/implementations/P8000_ngc25.04_nemo/env.sh
@@ -0,0 +1,5 @@
+export DATADIR="/mnt/mlperf/llama2_70b_lora_data/gov_report"
+export MODEL="/mnt/mlperf/llama2_70b_lora_data/model"
+export LOGDIR="./results"
+export CONT_FILE="/mnt/sqsh/llama2_70b_lora_pyt.sqsh"
+export SLURM_MPI_TYPE=pmi2
