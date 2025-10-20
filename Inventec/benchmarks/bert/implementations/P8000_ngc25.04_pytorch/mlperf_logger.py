66eb669efefc87efc4d6990a29418419eb1d195f
JK Jung <jkjung13@gmail.com>
2025-10-20 15:35:36 +0800

    Remove Inventec/benchmarks/bert/implementations/P8000_ngc25.04_pytorch

--- Inventec/benchmarks/bert/implementations/P8000_ngc25.04_pytorch/mlperf_logger.py
+++
@@ -1,9 +0,0 @@
-# import collections
-# import os
-# import subprocess
-
-import torch
-from mlperf_common.logging import MLLoggerWrapper
-from mlperf_common.frameworks.pyt import PyTCommunicationHandler
-
-mllogger = MLLoggerWrapper(PyTCommunicationHandler())
