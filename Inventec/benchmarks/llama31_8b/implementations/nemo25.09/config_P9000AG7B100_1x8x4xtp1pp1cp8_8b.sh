60aea743364c0d2ebfbcdd6e7703e62114adf514
JK Jung <jkjung13@gmail.com>
2026-01-13 17:22:15 +0800

    Add llama31_8b config for P9000AG7B200

--- Inventec/benchmarks/llama31_8b/implementations/nemo25.09/config_P9000AG7B100_1x8x4xtp1pp1cp8_8b.sh
+++ Inventec/benchmarks/llama31_8b/implementations/nemo25.09/config_P9000AG7B100_1x8x4xtp1pp1cp8_8b.sh
@@ -1,42 +1,36 @@
 source $(dirname ${BASH_SOURCE[0]})/config_common.sh
-source $(dirname ${BASH_SOURCE[0]})/config_common_8b.sh
+source $(dirname ${BASH_SOURCE[0]})/config_common_fp4.sh
 source $(dirname ${BASH_SOURCE[0]})/config_common_cg.sh
+source $(dirname ${BASH_SOURCE[0]})/config_common_8b.sh
+source $(dirname ${BASH_SOURCE[0]})/config_common_fp8attn.sh
 
-# To be checked...
-unset NVTE_DPA_FP8_RECIPE
-unset NVTE_DPA_FP8DS_REDUCE_AMAX
-unset BUCKET_SIZE
-unset NVTE_DPA_FP8DS_REDUCE_AMAX
-export NVTE_NVFP4_DISABLE_STOCHASTIC_ROUNDING=1 #0
-export NVTE_NVFP4_DISABLE_2D_QUANTIZATION=1 #0
-
-export MINIBS=4
+export MINIBS=2
 export TENSOR_MODEL_PARALLEL=1
 export SEQ_PARALLEL=False
-
 export PIPELINE_MODEL_PARALLEL=1
 export INTERLEAVED_PIPELINE=null
-export CONTEXT_PARALLEL=8 #1
+export CONTEXT_PARALLEL=1
 
 export TP_COMM_OVERLAP=False
-export MICRO_BATCH_SIZE=1
+export MICRO_BATCH_SIZE=2
+export FULL_CUDA_GRAPH=0
 
-export WARMUP_STEPS=20
-export VAL_CHECK_INTERVAL=256
+export LR=0.0004
+export WARMUP_STEPS=16
+export VAL_CHECK_INTERVAL=768
 
-export LR=0.0004 #0.00008
 
 export DGXNNODES=1
 export DGXNGPU=8
 export DGXSYSTEM=$(basename $(readlink -f ${BASH_SOURCE[0]}) | sed 's/^config_//' | sed 's/\.sh$//' )
 
-export WALLTIME_RUNANDTIME=1440 #220
+export WALLTIME_RUNANDTIME=220
 export WALLTIME=$((5 + ${NEXP:-1} * ($WALLTIME_RUNANDTIME + 5)))
 
 # Inventec stuffs
 export MLPERF_SUBMITTER="Inventec Corporation"
 export MLPERF_SUBMISSION_ORG="Inventec Corporation"
-export MLPERF_SYSTEM_NAME="P8000G6H100"
-export MLPERF_SUBMISSION_PLATFORM="Inventec P8000G6H100"
+export MLPERF_SYSTEM_NAME="P9000AG7B200"
+export MLPERF_SUBMISSION_PLATFORM="Inventec P9000AG7B200"
 export MLPERF_STATUS="research"
-export GPU_ARCH=h100
+export GPU_ARCH=b200
