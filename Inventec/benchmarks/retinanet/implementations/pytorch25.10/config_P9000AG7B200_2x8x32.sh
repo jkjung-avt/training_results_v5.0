bf48bb99b3caa58312071629d616c6d61d6cc55f
JK Jung <jkjung13@gmail.com>
2026-01-19 08:49:30 +0800

    Add PLATFORM_SRUN_OPTIONS for P9000AG7

--- Inventec/benchmarks/retinanet/implementations/pytorch25.10/config_P9000AG7B200_2x8x32.sh
+++ Inventec/benchmarks/retinanet/implementations/pytorch25.10/config_P9000AG7B200_2x8x32.sh
@@ -28,6 +28,8 @@ export DGXSOCKETCORES=64
 export DGXNSOCKET=2
 export DGXHT=2  # HT is on is 2, HT off is 1
 
+export PLATFORM_SRUN_OPTIONS=( '--export=ALL,UCX_NET_DEVICES="mlx5_0:1,mlx5_1:1,mlx5_2:1,mlx5_3:1,mlx5_10:1,mlx5_11:1,mlx5_12:1,mlx5_13:1"' )
+
 # Inventec stuffs
 export MLPERF_SUBMITTER="Inventec Corporation"
 export MLPERF_SUBMISSION_ORG="Inventec Corporation"
